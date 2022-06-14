2e57b45a20dcb02fcda339d09b2ee32988e2d9ee
Ismail <3332190037@untirta.ac.id>
2022-06-14 22:57:32 +0700

    finalblomfix

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import pickle
+import Ngorbit1
+from PIL import Image
 # %matplotlib inline
-
-model = pickle.load(open('features.p','rb'))
 
 
 app = Flask(__name__)
@@ -27,9 +27,16 @@ def predict():
     image_path = "./images/"+imageFile.filename
     imageFile.save(image_path)
 
+    #classes = model.predict(images, batch_size=10)
+    classes = Ngorbit1.predict(image_path)
+    nama=classes[0]
+    alamat=classes[1]
+    jambuka=classes[2]
+    rating=classes[3]
+    ulasan=classes[4]
+    gambar=classes[5]
 
-
-    return render_template('index.html')
+    return render_template('index.html', nama=nama, alamat=alamat, jambuka=jambuka, rating=rating, ulasan=ulasan, gambar=gambar)
 
 if __name__ == '__main__':
     app.run(port=3000,debug=True)
